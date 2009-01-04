b5bb794b159fb93c476496623575a19dd05f1762
Jan Komzak <komzak@comz.cz>
2009-01-05 00:34:56 +0100

    updated migration for pages

--- db/migrate/20080902225218_create_pages.rb
+++ db/migrate/20080902225218_create_pages.rb
@@ -19,24 +19,24 @@ class CreatePages < ActiveRecord::Migration
       page.timestamps
       page.datetime :deleted_at
     end
-    
-    # VERSIONS
-	  Page.create_versioned_table
 
 		# INDEXES
 		say						"adding indexes ..."
 		add_index :pages, :title
 		add_index :pages, :slug
 		add_index :pages, :class_name
-
-		# INITIAL DATA
-		say						"loading initial data ..."
-		Page.create :title => 'Domů', :slug => '/', :body => '<h1>Prosím, uprav mne !</h1>', :layout_id => 1, :language => "cz", :status_id => 100
-		FileNotFoundPage.create :title => '404: Stránka nenalezena', :slug => 'stranka-nenalezena', :body => 'Hledaná stránka nebyla nalezena..', :layout_id => 1, :language => "cz", :status_id => 100
-    Css.create :title => 'Styl vzhledu', :slug => 'css', :body => 'body { color: white; background-color: black; }', :layout_id => 2, :language => "cz", :status_id => 100
-    SearchPage.create :title => 'Hledání', :slug => 'hledani', :body => '', :layout_id => 1, :language => "cz", :status_id => 100, :template_id => 2
     
     Page.transaction do
+      # VERSIONS
+	    Page.create_versioned_table
+	    
+  		# INITIAL DATA
+  		say						"loading initial data ..."
+  		Page.create :title => 'Domů', :slug => '/', :body => '<h1>Prosím, uprav mne !</h1>', :layout_id => 1, :language => "cz", :status_id => 100
+  		FileNotFoundPage.create :title => '404: Stránka nenalezena', :slug => 'stranka-nenalezena', :body => 'Hledaná stránka nebyla nalezena..', :layout_id => 1, :language => "cz", :status_id => 100
+      Css.create :title => 'Styl vzhledu', :slug => 'css', :body => 'body { color: white; background-color: black; }', :layout_id => 2, :language => "cz", :status_id => 100
+      SearchPage.create :title => 'Hledání', :slug => 'hledani', :body => '', :layout_id => 1, :language => "cz", :status_id => 100, :template_id => 2
+	    
 		  Page.all(:conditions => [ "id > 1" ]).each do |page|
 		    page.move_to_child_of(Page.find(1))
 		  end
