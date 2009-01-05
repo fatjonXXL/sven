1829aacf5429f4b4672a85a22f89fe0b197afab7
Jan Komzak <komzak@comz.cz>
2009-01-05 02:03:57 +0100

    updated migrations

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -3,7 +3,7 @@ class Notifier < ActionMailer::Base
 
   def password_reset_instructions( user )
     subject       "Zapomenute heslo"
-    from          "svenMS"
+    from          "sven@comz.cz"
     recipients    user.email
     sent_on       Time.now
     body          :edit_password_reset_url => edit_password_reset_url(user.perishable_token), :sven_host => admin_url, :admin_mail => User.first.email
@@ -11,7 +11,7 @@ class Notifier < ActionMailer::Base
   
   def email_with_password( user, password )
     subject       "Přihlasovaci udaje"
-    from          "svenMS"
+    from          "sven@comz.cz"
     recipients    user.email
     sent_on       Time.now
     body          :password => password
